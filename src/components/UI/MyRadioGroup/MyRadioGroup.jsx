b1b162baa232e9f86e1a7a66919b7eab792c0b54
p3rfect <nikita.galukh@yandex.ru>
2023-05-24 13:25:15 +0300

    control fix

--- src/components/UI/MyRadioGroup/MyRadioGroup.jsx
+++ src/components/UI/MyRadioGroup/MyRadioGroup.jsx
@@ -3,15 +3,17 @@ import {FormControl, FormLabel, Radio, RadioGroup} from "@mui/material";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import classes from "./MyRadioGroup.module.css"
 
-const MyRadioGroup = ({options, label, value, handleChange}) => {
+const MyRadioGroup = ({id, options, label, value, handleChange}) => {
     return (
-        <FormControl className={classes.MyRadioGroup}>
+        <FormControl className={classes.MyRadioGroup} id={id}>
             <FormLabel id="radio-group-label" className={classes.Label}>{label}</FormLabel>
             <RadioGroup
                 aria-labelledby="radio-group-label"
                 value={value}
-                onChange={handleChange !== undefined ? (e => handleChange(e)) : null}
+                onChange={e => handleChange(e)}
                 className={classes.RadioGroup}
+                id={id}
+                name={id}
             >
                 {options.map((option, index) => (
                     <FormControlLabel control={<Radio/>} label={option} value={option} key={`radio-group-element${index}`}/>
--- src/components/UI/MySelect/MySelect.jsx
+++ src/components/UI/MySelect/MySelect.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import classes from "./MySelect.module.css";
-import {FormControl, FormHelperText, InputLabel, MenuItem} from "@mui/material";
+import {FormControl, InputLabel, MenuItem} from "@mui/material";
 import Select from "@mui/material/Select";
 
-const MySelect = ({options, label, value, handleChange}) => {
+const MySelect = ({id, options, label, value, handleChange}) => {
     return (
         <FormControl className={classes.MySelect}>
             <InputLabel id={`select-label`} value={label}>{label}</InputLabel>
             <Select
                 labelId={`select-label`}
-                name={'select'}
                 value={value}
                 onChange={handleChange !== undefined ? (e => handleChange(e)) : null}
                 label={label}
+                name={id}
             >
                 {options.map((option, index) => (
                     <MenuItem key={`select-option${index}`} value={option}>{option}</MenuItem>
